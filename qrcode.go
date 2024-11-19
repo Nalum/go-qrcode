d4c2e0f8da5676696b58b2953dcda32f3d2197fb
Luke Mallon (Nalum) <luke@mallon.ie>
2024-11-19 21:21:31 +0000

    Fix includes
    Signed-off-by: Luke Mallon (Nalum) <luke@mallon.ie>

--- qrcode.go
+++ qrcode.go
@@ -60,8 +60,8 @@ import (
 	"log"
 	"os"
 
-	bitset "github.com/skip2/go-qrcode/bitset"
-	reedsolomon "github.com/skip2/go-qrcode/reedsolomon"
+	bitset "github.com/nalum/go-qrcode/bitset"
+	reedsolomon "github.com/nalum/go-qrcode/reedsolomon"
 )
 
 // Encode a QR Code and return a raw PNG image.
